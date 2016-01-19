ec1f23da603030587f3f4908223be9cc77c89fd9
chenxing12 <woshimrf@126.com>
2016-01-19 10:11:36 +0800

    Initial commit

---
+++ mybatis-spring/src/test/java/ssm/mapper/UserMapperTest.java
@@ -0,0 +1,28 @@
+package ssm.mapper;
+
+import cn.itcast.ssm.mapper.UserMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.itcast.ssm.po.User;
+
+public class UserMapperTest {
+
+	private ApplicationContext applicationContext;
+
+	//在setUp这个方法得到spring容器
+	@Before
+	public void setUp() throws Exception {
+		applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
+	}
+
+	@Test
+	public void testFindUserById() throws Exception {
+		UserMapper userMapper = (UserMapper) applicationContext.getBean("userMapper");
+		User user = userMapper.findUserById(1);
+		System.out.println(user);
+	}
+
+}
